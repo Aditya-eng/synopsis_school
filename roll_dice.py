7f96b6c0ed684eb101f8a5a4f967fc3ffbb05944
Aditya garg <58650336+Aditya-eng@users.noreply.github.com>
2022-01-06 22:40:46 +0530

    Update roll_dice.py

--- roll_dice.py
+++ roll_dice.py
@@ -1,5 +1,5 @@
 import random
-inp1 = input("If you want to roll the dice type 'ROLL', if not type 'CHOOSE': ")
+inp1 = input("Let's start the game Enter any Key: ")
 rndm = random.randint(1, 6)
 r1=0
 
@@ -7,6 +7,9 @@ def email_slicer():
     for i in range(10):
         print("You're really lucky. You got the chance to slice your email!! ")
         email = input("Input your EMAIL ID to slice it!: ").strip()
+        if "@" not in email:
+            print("NOT")
+            break
         username = email[:email.index('@')]
         domain = email[email.index('@') + 1:]
         print("Your username is ", username, " & domain is ", domain)
@@ -16,6 +19,7 @@ def email_slicer():
         else:
             print("STOPPED SUCCESSFULLY!(you typed STOP or something else)")
             break
+
 
 def guess_the_word():
     print("OMG! It's GUESS THE WORD.")
@@ -90,14 +94,55 @@ def guess_the_word():
             if turns == 0:
                 print("You Loose")
 
-def currency_converter():
-    print('Welcome to the currency converter!!!😃')
-    cinp1 = input("Enter the country's name(currency) you want to convert , Eg[INDIA]: ")
-    cinp2 = input("Enter the currency you want to convert to, Eg[USA]: ")
-    # l1 =
+def countdown_calculator():
+    # for i in range(10):
+    #     print("You're really lucky. You got the chance to slice your email!! ")
+    #     email = input("Input your EMAIL ID to slice it!: ").strip()
+    #     if "@" not in email:
+    #         print("NOT")
+    #         break
+    #     username = email[:email.index('@')]
+    #     domain = email[email.index('@') + 1:]
+    #     print("Your username is ", username, " & domain is ", domain)
+    #     email1 = input("If you want to slice another email press 'cont' ,if not type 'STOP': ")
+    #     if email1 == "cont":
+    #         continue
+    #     else:
+    #         print("STOPPED SUCCESSFULLY!(you typed STOP or something else)")
+    #         break
+
+    a, b, c = [int(x) for x in input("Enter the starting date:").split()]
+    print("Date is ", a, b, c)
+
+    d, e, f = [int(x) for x in input("Enter the starting date:").split()]
+    print("Date is ", d, e, f)
+
+    year = f - c
+    month = e - b
+    day = d - a
+
+    var1 = 1
+    var2 = 0
+
+    if month < 0:
+        var1 = var1 * 12 - abs(month)
+        year -= 1
+        month = var1
+        print(day, "Day", month, "-month", year, "-Year")
+        if day < 0:
+            var2 = var2 * 30 - abs(day)
+            day = var2
+            month -= 1
+            print(day, "Day", month, "-month", year, "-Year")
+        else:
+            print(day, "Day", month, "-month", year, "-Year")
+    else:
+        print(day, "Day", month, "-month", year, "-Year")
+
 
 def fibonacci_checker():
     for i in range(10):
+        print()
         print("Welcome to Find out Fibonacci!!")
         print("You'll have to give a number and the program will check if its in fibonacci.")
         # A number is Fibonacci if and only if one or both of (5*n^2 + 4) or (5*n^2 – 4) is a perfect square
@@ -116,8 +161,8 @@ def fibonacci_checker():
         if fib1 == 0:
             print("STOPPED.")
             break
-        else:
-            print("Not Fibonacci !")
+        elif (fib2).is_integer() == False or (fib4).is_integer() == False:
+            print("NOT FIBONACCI")
 
 def rock_paper_scissors():
     rpslist = ["R", "P", "S"]
@@ -171,13 +216,14 @@ def leap_it():
 
 
 
-######FINAL CODE################
+###### FINAL CODE ################
 
-if inp1.upper() == "ROLL":
+if type(inp1.upper()) == type(str()) or type(inp1.upper()) == type(int()):
         rndm = random.randint(1, 6)
         r1 = rndm
         print("The dice has been rolled!")
         print("And the number is ", r1)
+        print()
 
         if r1 == 1:
             email_slicer()
@@ -196,7 +242,7 @@ if inp1.upper() == "ROLL":
             print()
 
         else:
-            print("6th")
+            currency_converter()
 # elif r1.upper() == "CHOOSE":
 #
 
