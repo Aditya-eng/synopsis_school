c740212c46cc25c09afddf1641d8361c8fe0ebb4
Aditya garg <58650336+Aditya-eng@users.noreply.github.com>
2022-03-04 13:44:17 +0530

    Update roll_dice.py

--- roll_dice.py
+++ roll_dice.py
@@ -1,13 +1,14 @@
 import random
-inp1 = input("Let's start the game Enter any Key: ")
+inp1 = input("Let's start the game, Enter your name : ")
 rndm = random.randint(1, 6)
 r1=0
 
 def email_slicer():
     for i in range(10):
         print("You're really lucky. You got the chance to slice your email!! ")
-        email = input("Input your EMAIL ID to slice it!: ").strip()
-        if "@" not in email:
+        email1 = input("Input your EMAIL ID to slice it!: ")
+        email = email1.strip()
+        if "@" not in email1:
             print("@ is not there.")
         username = email[:email.index('@')]
         domain = email[email.index('@') + 1:]
@@ -22,8 +23,6 @@ def email_slicer():
 def guess_the_word():
     print("OMG! It's GUESS THE WORD.")
 
-    # library that we use in order to choose
-    # on random words from a list of words
 
     name = input("What is your name? ")
     # Here the user is asked to enter the name first
@@ -34,24 +33,20 @@ def guess_the_word():
              'lion', 'mavericks', 'yosemite', 'elcaptain',
              'sierra', 'mojave', 'catalina', 'bigsur','monetery']
 
-    # Function will choose one random
-    # word from this list of words
+
     word = random.choice(words)
 
     print("Guess the characters")
 
     guesses = ''
 
-    # any number of turns can be used here
+
     turns = 12
 
     while turns > 0:
 
-        # counts the number of times a user fails
         failed = 0
 
-        # all characters from the input
-        # word taking one at a time.
         for char in word:
 
             char1 = len(char)
@@ -60,22 +55,17 @@ def guess_the_word():
 
             else:
                 print("_")
-
-                # for every failure 1 will be
-                # incremented in failure
                 failed += 1
 
         if failed == 0:
-            # user will win the game if failure is 0
-            # and 'You Win' will be given as output
+
             print("You Win")
 
             # this print the correct word
             print("The word is: ", word)
             break
 
-        # if user has input the wrong alphabet then
-        # it will ask user to enter another alphabet
+
         guess = input("guess a character:")
 
         guesses += guess
@@ -85,12 +75,12 @@ def guess_the_word():
 
             turns -= 1
 
-            # if the character doesn’t match the word
             print("Wrong")
             print("You have", + turns, 'more guesses')
 
             if turns == 0:
                 print("You Loose")
+                print("The word was:",)
 
 def countdown_calculator():
     # for i in range(10):
@@ -146,22 +136,27 @@ def fibonacci_checker():
         print("You'll have to give a number and the program will check if its in fibonacci.")
         # A number is Fibonacci if and only if one or both of (5*n^2 + 4) or (5*n^2 – 4) is a perfect square
         fib1 = int(input("Enter the number (0 for stopping): "))
-        fib3 = fib1 * fib1 * 5 + 4
-        fib5 = fib1 * fib1 * 5 - 4
-        fib4 = fib5 ** 0.5
-        # fib2 = math.isqrt(fib3)
-        fib2 = fib3 ** 0.5
-        # print(fib3, fib2)
-        # print(fib2.is_integer())
-        if (fib2).is_integer() == True or (fib4).is_integer() == True:
-            print(fib1, "is a fibonacci number.")
-            # print(fib1, fib2, fib3)
+        if type(fib1) == str():
+            print("Not a number.")
+        elif type(fib1) == int():
+            fib3 = fib1 * fib1 * 5 + 4
+            fib5 = fib1 * fib1 * 5 - 4
+            fib4 = fib5 ** 0.5
+            # fib2 = math.isqrt(fib3)
+            fib2 = fib3 ** 0.5
+            # print(fib3, fib2)
+            # print(fib2.is_integer())
+            if (fib2).is_integer() == True or (fib4).is_integer() == True:
+                print(fib1, "is a fibonacci number.")
+                # print(fib1, fib2, fib3)
 
-        if fib1 == 0:
-            print("STOPPED.")
-            break
-        elif (fib2).is_integer() == False or (fib4).is_integer() == False:
-            print("NOT FIBONACCI")
+            if fib1 == 0:
+                print("STOPPED.")
+                break
+            elif (fib2).is_integer() == False or (fib4).is_integer() == False:
+                print("NOT FIBONACCI")
+
+
 
 def rock_paper_scissors():
     rpslist = ["R", "P", "S"]
@@ -199,8 +194,6 @@ def rock_paper_scissors():
     else:
         print("NOT A NUMBER")
 
-
-
 def leap_it():
 
     for i in range(10):
@@ -221,36 +214,31 @@ def leap_it():
                 print(li, " is not a Leap Year.")
 
 
-
-
-
 ###### FINAL CODE ################
 
 if type(inp1.upper()) == type(str()) or type(inp1.upper()) == type(int()):
         rndm = random.randint(1, 6)
         r1 = rndm
-        print("The dice has been rolled!")
+        print("The dice has been rolled ",inp1)
         print("And the number is ", r1)
         print()
 
         if r1 == 1:
             email_slicer()
 
-        elif r1==7 :
+        elif r1==2 :
             guess_the_word()
 
-        elif r1==7:
+        elif r1==3:
             fibonacci_checker()
 
-        elif r1==7 :
+        elif r1==4 :
             rock_paper_scissors()
 
-        elif r1==7:
+        elif r1==5:
             leap_it()
             print()
 
         else:
-            email_slicer()
-            # countdown_calculator()
-
-
+            countdown_calculator()            # countdown_calculator()
+# elif r1.upper() == "CHOOSE":
